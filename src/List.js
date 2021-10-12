abedc5223ea6e6d5c41a7ae60c9d273ed4c1a502
Kevin Canlas <kvncnlsdsgns@gmail.com>
2021-10-12 08:38:33 -0400

    Updated styling, added comments.

--- src/List.js
+++ src/List.js
@@ -4,14 +4,18 @@ const List = ({people, removeBirthday}) => {
   return (
     <>
       {people.map((person)=>{
+        // destructuring each 'person'
         const {id, name, age, image} = person;
         return (
+          // key attribute is required for list items for specificity
+          // Here it's linked to each person's unique id.
             <article key={id} className="person">
               <img src={image} alt={name}></img>
               <div className="info">
                 <h4>{name}</h4>
                 <p>{age} years</p>
               </div>
+              {/* Button removes the selected birthday */}
               <button className="remove-btn" onClick={()=>removeBirthday(id)}>Remove</button>
             </article>
         )

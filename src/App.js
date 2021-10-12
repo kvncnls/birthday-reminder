473413c4fe5cd3a81c82ea5bf313829aeb6761a5
Kevin Canlas <kvncnlsdsgns@gmail.com>
2021-10-12 08:35:19 -0400

    Added Remove button and updated styling.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import List from './List';
 function App() {
 
   // We import data from './data'
-  // and used it as an argument for the state 'people', passing the data
+  // and pass it as an argument for the state.
   const [people, setPeople] = useState(data)
+
+  // Removes the birthday
+  const removeBirthday = (id) => {
+    const filteredBirthdays = people.filter((person) => person.id !== id)
+    setPeople(filteredBirthdays)
+  }
 
   return <main>
     <section className="container">
+      {/* If the number of listed persons is NOT equal to 1,
+      return "# birthdays today", otherwise, return "# birthday today" */}
       <h3>{people.length !== 1 ? `${people.length} birthdays today` : `${people.length} birthday today`}</h3>
-      <List people={people}/>
-      {/* Here we passed an empty array to the setPeople state function, clearing the list. */}
-      <button onClick={()=> setPeople([])}>Clear All</button>
+      {/* Component with prop "people" taking in the state "people" */}
+      <List people={people} removeBirthday={removeBirthday}/>
+      {/* Here we passed an empty array to the setPeople state method, clearing the list. */}
+      <button onClick={()=> setPeople([])} >Clear All</button>
     </section>
   </main>
 }
--- src/List.js
+++ src/List.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const List = ({people}) => {
+const List = ({people, removeBirthday}) => {
   return (
     <>
       {people.map((person)=>{
         const {id, name, age, image} = person;
-        return <article key={id} className="person">
-          <img src={image} alt={name}></img>
-          <div>
-            <h4>{name}</h4>
-            <p>{age} years</p>
-          </div>
-        </article>
+        return (
+            <article key={id} className="person">
+              <img src={image} alt={name}></img>
+              <div className="info">
+                <h4>{name}</h4>
+                <p>{age} years</p>
+              </div>
+              <button className="remove-btn" onClick={()=>removeBirthday(id)}>Remove</button>
+            </article>
+        )
       })}
     </>
   );
